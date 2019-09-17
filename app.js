50c5b9aaabc94b9d5980b2a7e644f3b9480840dc
Thiago Leite <thiagofmleite@gmail.com>
2019-09-17 12:05:43 -0300

    Serving static files

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const app = express()
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Serves statics files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Admin Routes
 app.use('/admin', adminRoutes)
+
+// Public Routes
 app.use(shopRoutes)
 
+// 404 Page
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
+
+// Start server on port 3000
 app.listen(3000)

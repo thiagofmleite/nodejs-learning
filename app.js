0f876aab1c3318bc0a79b469bcbef8cee47e4f2e
Thiago Leite <thiagofmleite@gmail.com>
2019-09-17 16:42:29 -0300

    Convert all pages to Pug Added iterators with pug pages

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.use(shopRoutes)
 
 // 404 Page
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: 'Page not found' })
 })
 
 // Start server on port 3000

2c176ebf1cd8cb5bb2e3f728701fba607566fe28
Thiago Leite <thiagofmleite@gmail.com>
2019-09-26 14:30:43 -0300

    [MVC] Using Model to store and fetch data in files

--- models/product.js
+++ models/product.js
@@ -1,15 +1,51 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+)
+
+/**
+ * Return an array of Products from a JSON file
+ *
+ * @param {} cb Callback Function
+ */
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([])
+    }
+    cb(JSON.parse(fileContent))
+  })
+}
 
 module.exports = class Product {
   constructor(t) {
     this.title = t
   }
 
+  /**
+   * Save a Product
+   *
+   */
   save() {
-    products.push(this)
+    getProductsFromFile(products => {
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.error(err)
+      })
+    })
   }
 
-  static fetchAll() {
-    return products
+  /**
+   * Fetch all products
+   *
+   * @static
+   * @param {*} callback Callback Function
+   */
+  static fetchAll(callback) {
+    getProductsFromFile(callback)
   }
 }

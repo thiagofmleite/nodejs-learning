40e00afac2d1c06b965adb020560c4693b9bd78c
Thiago Leite <thiagofmleite@gmail.com>
2019-09-26 13:59:35 -0300

    [MVC] Split responsabilities with Models

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = []
+const Product = require('../models/product')
 
 exports.getAddProdct = (req, res, next) => {
   res.render('add-product', {
@@ -11,13 +11,13 @@ exports.getAddProdct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({
-    title: req.body.title
-  })
+  const product = new Product(req.body.title)
+  product.save()
   res.redirect('/')
 }
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll()
   res.render('shop', {
     prods: products,
     pageTitle: 'Shop',

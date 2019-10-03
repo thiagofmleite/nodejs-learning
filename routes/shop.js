877fe21c3f2e589dc5b5cd5e0a2e393abfb076b1
Thiago Leite <thiagofmleite@gmail.com>
2019-10-03 10:44:50 -0300

    Cart Model

--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,7 @@ router.get('/', controller.getIndex)
 router.get('/products/:productId', controller.getProduct)
 router.get('/products', controller.getProducts)
 router.get('/cart', controller.getCart)
+router.post('/cart', controller.postCart)
 router.get('/orders', controller.getOrders)
 router.get('/checkout', controller.getCheckout)
 
